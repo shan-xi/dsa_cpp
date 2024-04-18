7b652772d2177117ee71dfbf7a20eedc9b45cd12
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2024-04-18 12:33:28 +0800

    leetcode:71. Simplify Path

--- dsa_cpp/leecode/15_3Sum/Solution.cpp
+++ dsa_cpp/leecode/15_3Sum/Solution.cpp
@@ -45,50 +45,50 @@ public:
     }
 };
 
-int main() {
-    Solution s;
-
-    vector<int> nums1 = {-1, 0, 1, 2, -1, -4};
-    vector<vector<int>> result1 = s.threeSum(nums1);
-    cout << "[";
-    for (int i = 0; i < result1.size(); i++) {
-        cout << "[";
-        for (int j = 0; j < result1[i].size(); j++) {
-            cout << result1[i][j];
-            if (j < result1[i].size() - 1) cout << ",";
-        }
-        cout << "]";
-        if (i < result1.size() - 1) cout << ",";
-    }
-    cout << "]" << endl; // [[-1,-1,2],[-1,0,1]]
-
-    vector<int> nums2 = {0, 1, 1};
-    vector<vector<int>> result2 = s.threeSum(nums2);
-    cout << "[";
-    for (int i = 0; i < result2.size(); i++) {
-        cout << "[";
-        for (int j = 0; j < result2[i].size(); j++) {
-            cout << result2[i][j];
-            if (j < result2[i].size() - 1) cout << ",";
-        }
-        cout << "]";
-        if (i < result2.size() - 1) cout << ",";
-    }
-    cout << "]" << endl; // []
-
-    vector<int> nums3 = {0, 0, 0};
-    vector<vector<int>> result3 = s.threeSum(nums3);
-    cout << "[";
-    for (int i = 0; i < result3.size(); i++) {
-        cout << "[";
-        for (int j = 0; j < result3[i].size(); j++) {
-            cout << result3[i][j];
-            if (j < result3[i].size() - 1) cout << ",";
-        }
-        cout << "]";
-        if (i < result3.size() - 1) cout << ",";
-    }
-    cout << "]" << endl; // [[0,0,0]]
-
-    return 0;
-}
+//int main() {
+//    Solution s;
+//
+//    vector<int> nums1 = {-1, 0, 1, 2, -1, -4};
+//    vector<vector<int>> result1 = s.threeSum(nums1);
+//    cout << "[";
+//    for (int i = 0; i < result1.size(); i++) {
+//        cout << "[";
+//        for (int j = 0; j < result1[i].size(); j++) {
+//            cout << result1[i][j];
+//            if (j < result1[i].size() - 1) cout << ",";
+//        }
+//        cout << "]";
+//        if (i < result1.size() - 1) cout << ",";
+//    }
+//    cout << "]" << endl; // [[-1,-1,2],[-1,0,1]]
+//
+//    vector<int> nums2 = {0, 1, 1};
+//    vector<vector<int>> result2 = s.threeSum(nums2);
+//    cout << "[";
+//    for (int i = 0; i < result2.size(); i++) {
+//        cout << "[";
+//        for (int j = 0; j < result2[i].size(); j++) {
+//            cout << result2[i][j];
+//            if (j < result2[i].size() - 1) cout << ",";
+//        }
+//        cout << "]";
+//        if (i < result2.size() - 1) cout << ",";
+//    }
+//    cout << "]" << endl; // []
+//
+//    vector<int> nums3 = {0, 0, 0};
+//    vector<vector<int>> result3 = s.threeSum(nums3);
+//    cout << "[";
+//    for (int i = 0; i < result3.size(); i++) {
+//        cout << "[";
+//        for (int j = 0; j < result3[i].size(); j++) {
+//            cout << result3[i][j];
+//            if (j < result3[i].size() - 1) cout << ",";
+//        }
+//        cout << "]";
+//        if (i < result3.size() - 1) cout << ",";
+//    }
+//    cout << "]" << endl; // [[0,0,0]]
+//
+//    return 0;
+//}
---
+++ dsa_cpp/leecode/71_Simplify_Path/Solution.cpp
@@ -0,0 +1,55 @@
+//
+//  Solution.cpp
+//  dsa_cpp
+//
+//  Created by 廖善璽 on 2024/4/15.
+//
+
+#include <string>
+#include <stack>
+#include <sstream>
+#include <vector>
+#include <unordered_set>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stringstream ss(path);
+        string token;
+        stack<string> stack;
+        unordered_set<string> skip = {"..", ".", ""};
+        
+        while (getline(ss, token, '/')) {
+            if (token == ".." && !stack.empty()) {
+                stack.pop();
+            } else if (!skip.count(token)) {
+                stack.push(token);
+            }
+        }
+        
+        vector<string> result;
+        while (!stack.empty()) {
+            result.push_back(stack.top());
+            stack.pop();
+        }
+        
+        string simplifiedPath;
+        for (int i = static_cast<int>(result.size()) - 1; i >= 0; i--) {
+            simplifiedPath += "/" + result[i];
+        }
+        
+        return simplifiedPath.empty() ? "/" : simplifiedPath;
+    }
+};
+int main() {
+    Solution s;
+
+    cout << s.simplifyPath("/home/") << endl; // /home
+    cout << s.simplifyPath("/../") << endl; // /
+    cout << s.simplifyPath("/home//foo/") << endl; // /home/foo
+
+    return 0;
+}
